6bb2cabfd00c897a699b8ae77e76150e1ab54531
Taosheng <taosheng@shopback.com>
2018-01-29 08:05:08 +0000

    simple MapReduce for FB ID duplicate

---
+++ src/findDuplicateFB.js
@@ -0,0 +1,29 @@
+//USAGE mongo <dn name> findDuplicateFB.js
+//USAGE mongo SG2018-01-28 findDuplicateFB.js
+
+const mapper = function() {
+    if (this.socialMedia !== undefined  ){
+        emit( this.socialMedia.facebook.id, 1);
+    }
+}
+
+
+const reducer = function(key,values) {
+    var result = {
+         fbid : undefined,
+         count: 0
+    }
+
+    result.fbid = key ;
+    result.count= values.length ;
+    return result;
+}
+
+print("lets start");
+db.accounts.mapReduce(mapper, reducer, {out: { merge:"findDupMP"} } )
+print("done map-reduce");
+print("total FB");
+print(db.findDupMP.find({"value":1}).count());
+print("possibile FB duplicate")
+print(db.findDupMP.find( { "value": { $not : { $eq : 1} } }  ).count())
+
